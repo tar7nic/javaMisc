059b521ef5fc0d1e7b8d5dffee48ac21d9daf75d
tar7nic <154546879+tar7nic@users.noreply.github.com>
2025-09-21 17:32:48 +0530

    Add files via upload

---
+++ DispHindiChars.java
@@ -0,0 +1,7 @@
+public class DispHindiChars{
+    public static void main(String[] args) {
+        for (char c = '\u0900'; c<='\u097F'; c++){
+        System.out.print(c +" ");
+        }
+    }
+}
